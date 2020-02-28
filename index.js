225e54218d0d163d4e5fe82574d449db960fe130
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-02-27 21:38:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here
+const var1= 31
+
+const var2= 2
+
+const multiply= var1 * var2
+
+const random= Math.random()
+
+const equation= 13 % 9
+
+const mod= equation
+
+const max= (1, 3, 4, 20)
